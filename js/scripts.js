059a7425c3ffddd80ca0220b93f1281511802c6b
Jhony Almeida <jhony12358oficial@gmail.com>
2022-09-01 13:33:43 -0300

    SA2-Encontro Remoto 3

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function comprar() {
+    let ckick = document.getElementById("comprar-click").value
+    alert("Você acaba de Comprar este Produto!!!")
+    console.log("Você acaba de Comprar este Produto!!!")
+}
